891837b0fbc6bcdfa2a7f06caf494160607781b9
Christian Fischer <cf@fisch3r.de>
2016-10-18 09:40:46 +0200

    initial import

---
+++ src/test/java/de/agiledojo/tdd/cashregister/CashRegisterTest.java
@@ -0,0 +1,13 @@
+package de.agiledojo.tdd.cashregister;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CashRegisterTest {
+
+    @Test
+    public void testWorks() {
+        assertTrue(true);
+    }
+}
